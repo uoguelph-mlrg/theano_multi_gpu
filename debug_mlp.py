d3d9654302bed4300251fe25fe2edcb83f35d420
Weiguang Gavin Ding <gavin.w.ding@gmail.com>
2014-10-15 14:27:38 -0400

    added debug_mlp.py for finding the theao/pycuda conflict

---
+++ debug_mlp.py
@@ -0,0 +1,146 @@
+import os
+import sys
+import time
+
+import numpy
+
+from multiprocessing import Process
+
+import pycuda.driver as drv
+import pycuda.gpuarray as gpuarray
+
+
+def test_mlp(learning_rate=0.13, L1_reg=0.00, L2_reg=0.0001, n_epochs=10,
+             dataset='/mnt/data/datasets/mnist/mnist.pkl.gz',
+             batch_size=10000, n_hidden=500):
+
+    import theano.sandbox.cuda
+    theano.sandbox.cuda.use('gpu1')
+
+    import theano
+    import theano.tensor as T
+
+    from logistic_sgd import load_data
+    from mlp import MLP
+
+    import theano.misc.pycuda_init
+    import theano.misc.pycuda_utils
+
+    ########################
+    drv.init()
+    dev = drv.Device(1)
+    ctx = dev.make_context()
+    ########################
+
+    datasets = load_data(dataset)
+
+    train_set_x, train_set_y = datasets[0]
+    valid_set_x, valid_set_y = datasets[1]
+    test_set_x, test_set_y = datasets[2]
+
+    # compute number of minibatches for training, validation and testing
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
+    n_valid_batches = valid_set_x.get_value(borrow=True).shape[0] / batch_size
+
+    ######################
+    # BUILD ACTUAL MODEL #
+    ######################
+    print '... building the model'
+
+    # allocate symbolic variables for the data
+    index = T.lscalar()  # index to a [mini]batch
+    x = T.matrix('x')  # the data is presented as rasterized images
+    y = T.ivector('y')  # the labels are presented as 1D vector of
+    # [int] labels
+
+    rng = numpy.random.RandomState(1234)
+
+    # construct the MLP class
+    classifier = MLP(rng=rng, input=x, n_in=28 * 28,
+                     n_hidden=n_hidden, n_out=10)
+
+    # start-snippet-4
+    # the cost we minimize during training is the negative log likelihood of
+    # the model plus the regularization terms (L1 and L2); cost is expressed
+    # here symbolically
+    cost = (classifier.negative_log_likelihood(y)
+            + L1_reg * classifier.L1
+            + L2_reg * classifier.L2_sqr)
+    # end-snippet-4
+
+    validate_model = theano.function(
+        inputs=[index],
+        outputs=classifier.errors(y),
+        givens={x: valid_set_x[index * batch_size:(index + 1) * batch_size],
+                y: valid_set_y[index * batch_size:(index + 1) * batch_size]}
+    )
+
+    # start-snippet-5
+    # compute the gradient of cost with respect to theta (sotred in params)
+    # the resulting gradients will be stored in a list gparams
+    gparams = [T.grad(cost, param) for param in classifier.params]
+
+    # specify how to update the parameters of the model as a list of
+    # (variable, update expression) pairs
+
+    # given two list the zip A = [a1, a2, a3, a4] and B = [b1, b2, b3, b4] of
+    # same length, zip generates a list C of same size, where each element
+    # is a pair formed from the two lists :
+    #    C = [(a1, b1), (a2, b2), (a3, b3), (a4, b4)]
+    updates = [(param, param - learning_rate * gparam)
+               for param, gparam in zip(classifier.params, gparams)]
+
+    # compiling a Theano function `train_model` that returns the cost, but
+    # in the same time updates the parameter of the model based on the rules
+    # defined in `updates`
+    train_model = theano.function(
+        inputs=[index],
+        outputs=cost,
+        updates=updates,
+        givens={
+            x: train_set_x[index * batch_size: (index + 1) * batch_size],
+            y: train_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
+    # end-snippet-5
+
+    ###############
+    # TRAIN MODEL #
+    ###############
+    print '... training'
+
+    start_time = time.time()
+
+    epoch = 0
+    done_looping = False
+
+    while (epoch < n_epochs) and (not done_looping):
+        epoch = epoch + 1
+        for minibatch_index in xrange(n_train_batches):
+
+            train_model(minibatch_index)
+
+        validation_losses = [validate_model(i) for i
+                             in xrange(n_valid_batches)]
+        this_validation_loss = numpy.mean(validation_losses)
+
+        print('epoch %i, minibatch %i/%i, validation error %f %%' %
+              (epoch, minibatch_index + 1, n_train_batches,
+               this_validation_loss * 100.))
+
+    end_time = time.time()
+
+    print 'The code run for %d epochs, with %f epochs/sec' % (
+        epoch, 1. * epoch / (end_time - start_time))
+    print >> sys.stderr, ('The code for file ' +
+                          os.path.split(__file__)[1] +
+                          ' ran for %.1fs' % ((end_time - start_time)))
+
+
+if __name__ == '__main__':
+    # p = Process(target=test_mlp)
+
+    # p.start()
+    # p.join()
+
+    test_mlp()
